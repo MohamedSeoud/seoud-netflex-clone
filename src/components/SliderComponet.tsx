942eb2291004eadc63d09fbab10b3acaf07fd1e2
Mohammed Seoud <m.seoud42@gmail.com>
2023-09-30 01:10:06 +0300

    commit

--- src/components/SliderComponet.tsx
+++ src/components/SliderComponet.tsx
@@ -1,8 +1,11 @@
 import { Urls } from "../helpers/HttpUrls"
 import { MovieModel } from "../helpers/interface"
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 
 function SliderComponet({item}:{item:MovieModel}) {
+
+const liked = false
 
 
 
@@ -13,6 +16,25 @@ function SliderComponet({item}:{item:MovieModel}) {
     <div className=" justify-center w-[230px] h-[230px] items-center flex ">
      
       <img className=" w-[100%] h-[100%]" src={Urls.imageURL+item.backdrop_path} alt={item.title}/>
+      <div className=" absolute opacity-0 hover:opacity-100  w-[230px] h-[230px] duration-300
+       hover:bg-black/80  cursor-pointer">
+        <div className=" w-[100%] h-[100%] flex flex-col  items-center ">
+            <span className={`w-[100%] p-6 h-fit flex ${liked?"text-red-600":"text-white"} justify-start items-start`}>
+                { !liked ?<AiOutlineHeart size='2.5rem'/>
+                  : <AiFillHeart size='2.5rem' />
+                }
+
+            </span>
+            <span className="w-[100%] p-2 h-fit flex justify-center items-start">
+                {item.title}
+            </span>
+            <span className="w-[100%] uppercase p-2 h-fit flex justify-center items-start">
+                rate : {item.vote_average}
+            </span>
+
+
+        </div>
+       </div>
     </div>
   )
 }

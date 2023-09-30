de06375f4d0d5eb6503a0f71545a01044d7cfe20
Mohammed Seoud <m.seoud42@gmail.com>
2023-10-01 01:35:41 +0300

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   const{email,Logout} = UserAuth()
   return (
     <div className='w-full z-10 fixed flex flex-row py-4  gap-y-2 px-8  flex-shrink justify-between'>
-        <div className=' text-red-600 uppercase font-semibold text-4xl sm:px-4'> netflix</div>
+        <Link to={'/'} className=' cursor-pointer text-red-600 uppercase font-semibold text-4xl sm:px-4'> netflix</Link>
         <div className="px-4">
             <button className=' w-fit px-4 py-2 bg-red-700 rounded-xl font-medium text-sm uppercase 
              border-2 border-red-700 hover:bg-transparent text-white '>
